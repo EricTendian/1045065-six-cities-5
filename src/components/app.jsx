0596ab879bf0c41bdaa1a7216c4764a4588bdcb1
Eric Tendian <erictendian@gmail.com>
2020-10-05 22:48:38 -0500

    Completing task 1 in module 2: creating routing

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-import Main from "./main";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import PropTypes from "prop-types";
+import Main from "./main";
+import Login from "./login";
+import Favorites from "./favorites";
+import Property from "./property";
 
 const App = (props) => {
-  return <Main placesCount={props.placesCount} />;
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Main placesCount={props.placesCount} />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/favorites">
+        <Favorites />
+      </Route>
+      <Route path="/offer/:id?">
+        <Property />
+      </Route>
+    </Switch>
+  </BrowserRouter>;
 };
 
 App.propTypes = {
